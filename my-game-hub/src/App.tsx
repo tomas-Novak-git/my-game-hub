348dc9949fe31af19a3b64a49865d859bddfe749
Yatrikoxi <tomas_novak@centrum.cz>
2023-07-12 13:09:20 +0200

    Build a responsive layout

--- my-game-hub/src/App.tsx
+++ my-game-hub/src/App.tsx
@@ -1,9 +1,37 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import { useState } from "react";
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem
+        area='nav'
+        bg='coral'
+      >
+        Nav
+      </GridItem>
+      <Show above='lg'>
+        <GridItem
+          area='aside'
+          bg='gold'
+        >
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem
+        area='main'
+        bg='dodgerblue'
+      >
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
