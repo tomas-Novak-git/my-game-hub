6b8f9a1d40aaa56045b34a0faa052fcc59366e20
Yatrikoxi <tomas_novak@centrum.cz>
2023-07-13 11:43:14 +0200

    fetch the games

--- my-game-hub-react/src/App.tsx
+++ my-game-hub-react/src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import { useState } from "react";
 import "./App.css";
+import GameGrid from "./components/GameGrid";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -17,7 +18,9 @@ function App() {
       <Show above='lg'>
         <GridItem area='aside'>Aside</GridItem>
       </Show>
-      <GridItem area='main'>Main</GridItem>
+      <GridItem area='main'>
+        <GameGrid />
+      </GridItem>
     </Grid>
   );
 }
