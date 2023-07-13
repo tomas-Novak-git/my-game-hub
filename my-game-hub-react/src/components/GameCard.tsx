a92c57e652339a888e4d6fc8c6d81dd849d1c5be
Yatrikoxi <tomas_novak@centrum.cz>
2023-07-13 12:36:01 +0200

    Displaying Platform Icons

--- my-game-hub-react/src/components/GameCard.tsx
+++ my-game-hub-react/src/components/GameCard.tsx
@@ -1,6 +1,8 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
+import PlatformIcon from "./PlatformIconList";
 interface Props {
   game: Game;
 }
@@ -14,6 +16,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
