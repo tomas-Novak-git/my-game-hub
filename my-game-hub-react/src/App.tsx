3ac026fe3f64bf9f336cfb6b1dd84770e5dc1d6a
Yatrikoxi <tomas_novak@centrum.cz>
2023-07-14 10:36:14 +0200

    Sort games

--- my-game-hub-react/src/App.tsx
+++ my-game-hub-react/src/App.tsx
@@ -12,6 +12,7 @@ import { Genre } from "./hooks/useGenres";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 function App() {
@@ -53,7 +54,12 @@ function App() {
               setGameQuery({ ...gameQuery, platform })
             }
           />
-          <SortSelector />
+          <SortSelector
+            sortOrder={gameQuery.sortOrder}
+            onSelectSortOrder={(sortOrder) =>
+              setGameQuery({ ...gameQuery, sortOrder })
+            }
+          />
         </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
--- my-game-hub-react/src/services/image-url.ts
+++ my-game-hub-react/src/services/image-url.ts
@@ -1,5 +1,7 @@
 const getCroppedImageUrl = (url : string) => {
-   const target = 'media/';
+   if(!url) return '';
+   
+    const target = 'media/';
     const index =  url.indexOf ('media/') + 'media/'.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
