409cf628d53bb2b54acd396a48bb2cff648db740
Yatrikoxi <tomas_novak@centrum.cz>
2023-07-14 10:59:14 +0200

    implement searching

--- my-game-hub-react/src/App.tsx
+++ my-game-hub-react/src/App.tsx
@@ -13,6 +13,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -29,7 +30,9 @@ function App() {
       }}
     >
       <GridItem area='nav'>
-        <NavBar />
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
       <Show above='lg'>
         <GridItem
